44893545d3a0a9013f16ec75a234fde9a0848bda
Faris Rayhan <frayhan94@gmail.com>
2021-10-31 14:05:53 +0700

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,181 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import mockData from './mockup';
 import './App.css';
 
 function App() {
+  const [activeCourse, setActiveCourse] = useState(null);
+
+  const tab = {
+    paddingRight: '20px',
+    paddingLeft: '20px',
+    cursor: 'pointer',
+    fontFamily: 'Lato, sans-serif'
+  }
+
+  const tabFont = {
+    marginBottom: '10px',
+    fontWeight: '800',
+    marginTop: '10px'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', paddingTop: '30px', justifyContent: 'center'}}>
+      <div style={{ paddingRight: '20px', flexBasis: '50%'}}>
+        <div>
+          {
+            activeCourse && activeCourse.source ? (
+                <iframe
+                    width={'100%'}
+                    height={'300px'}
+                    src={activeCourse.source}
+                />
+            ) : (
+                <iframe
+                    width={'100%'}
+                    height={'300px'}
+                    src={'http://techslides.com/demos/sample-videos/small.mp4'}
+                />
+            )
+          }
+        </div>
+        <div style={{ display: 'flex'}}>
+          <div style={tab}>
+            <div style={tabFont}>
+              Overview
+            </div>
+            {
+              activeCourse && activeCourse.overview ? (
+                  <div>
+                    {activeCourse.overview}
+                  </div>
+              ) : (
+                  <div>
+                    -
+                  </div>
+              )
+            }
+          </div>
+          <div style={tab}>
+            <div style={tabFont}>
+              Q and A
+            </div>
+            {
+              activeCourse && activeCourse.qAndA ? activeCourse.qAndA.map((valueQAndA) => {
+                return (
+                    <div>
+                      {valueQAndA.value}
+                    </div>
+                )
+              }) : (
+                  <div>
+                    -
+                  </div>
+              )
+            }
+          </div>
+          <div style={tab}>
+            <div style={tabFont}>
+              Notes
+            </div>
+            {
+              activeCourse && activeCourse.notes ? activeCourse.notes.map((valueNotes) => {
+                return (
+                    <div>
+                      {valueNotes.value}
+                    </div>
+                )
+              }) : (
+                  <div>
+                    -
+                  </div>
+              )
+            }
+          </div>
+          <div style={tab}>
+            <div style={tabFont}>
+              Announcement
+            </div>
+            {
+              activeCourse && activeCourse.announcement ? (
+                  <div>
+                    activeCourse.announcement
+                  </div>
+              ) : (
+                  <div>
+                    -
+                  </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'space-between'}}>
+        <div style={{
+          paddingLeft: '15px',
+          paddingRight: '15px',
+          boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
+        }}>
+          <div>
+            <div style={{
+              paddingTop: '20px',
+              paddingBottom: '20px',
+              display: 'flex',
+              justifyContent: 'space-between'
+            }}>
+              <div>
+                Course Content
+              </div>
+              <div>
+                X
+              </div>
+            </div>
+            {
+              mockData.map((value) => {
+                return (
+                    <div>
+                      <div
+                          style={{
+                            paddingTop: '20px',
+                            paddingBottom: '20px',
+                            fontWeight: '800'
+                          }}
+                      >
+                        {value.sectionName}
+                      </div>
+                      {
+                        value.course.map((valueCourse) => {
+                          return (
+                              <div style={{
+                                cursor: 'pointer',
+                                paddingLeft: '15px',
+                                paddingRight:'15px',
+                                paddingTop: '20px',
+                                paddingBottom: '20px',
+                                textAlign:'left',
+                                fontFamily: 'Lato, sans-serif'
+                              }}
+                                   onClick={() => {
+                                     setActiveCourse({
+                                       announcement: value.announcement,
+                                       notes:value.notes,
+                                       qAndA: value.qAndA,
+                                       overview: valueCourse.name,
+                                       source: valueCourse.source
+                                     })
+                                   }}
+                              >
+                                {valueCourse.name}
+                              </div>
+                          )
+                        })
+                      }
+                    </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/mockup.js
@@ -0,0 +1,151 @@
+const mockData = [
+    {
+        sectionName: 'Section 3: Swarm intro and creating a 3 node swarm cluster',
+        overView: 'This is overview',
+        qAndA: [
+            {
+                id:1,
+                value: 'what is the meaning of Q ?'
+            },
+            {
+                id:2,
+                value: 'what is the meaning of A ?'
+            },
+            {
+                id:3,
+                value: 'what is the meaning of Q and A ?'
+            },
+            {
+                id:4,
+                value: 'what is the meaning of docker ?'
+            }
+        ],
+        notes: [
+            {
+                id:1,
+                value: 'first note'
+            },
+            {
+                id:2,
+                value: 'second note'
+            },
+            {
+                id:3,
+                value: 'third note'
+            },
+            {
+                id:4,
+                value: 'fourth note'
+            }
+        ],
+        announcement: 'sample of announcement',
+        course: [
+            {
+                id: 6,
+                name: "Swarm Mode: Built in orchestration",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4'
+            },
+            {
+                id: 7,
+                name: "Create your first service",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'
+            },
+            {
+                id: 8,
+                name: "Update and delete service",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4'
+            },
+            {
+                id: 9,
+                name: "Docker machine with swarm",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
+            },
+            {
+                id: 10,
+                name: "Node swarm cluster",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+            },
+            {
+                id: 11,
+                name: "Using swarm visualizer",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+            }
+        ]
+    },
+    {
+        sectionName: 'Section 4: Swarm intro and creating a 3 node swarm cluster',
+        overView: 'This is overview',
+        qAndA: [
+            {
+                id:1,
+                value: 'what is the meaning of Q ?'
+            },
+            {
+                id:2,
+                value: 'what is the meaning of A ?'
+            },
+            {
+                id:3,
+                value: 'what is the meaning of Q and A ?'
+            },
+            {
+                id:4,
+                value: 'what is the meaning of docker ?'
+            }
+        ],
+        notes: [
+            {
+                id:1,
+                value: 'first note'
+            },
+            {
+                id:2,
+                value: 'second note'
+            },
+            {
+                id:3,
+                value: 'third note'
+            },
+            {
+                id:4,
+                value: 'fourth note'
+            }
+        ],
+        announcement: 'sample of announcement',
+        course: [
+            {
+                id: 12,
+                name: "Swarm Mode: Built in orchestration",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+            },
+            {
+                id: 13,
+                name: "Create your first service",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+            },
+            {
+                id: 14,
+                name: "Update and delete service",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
+            },
+            {
+                id: 15,
+                name: "Docker machine with swarm",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4'
+            },
+            {
+                id: 16,
+                name: "Node swarm cluster",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'
+            },
+            {
+                id: 17,
+                name: "Using swarm visualizer",
+                source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4'
+            }
+        ]
+    }
+];
+
+
+export default mockData;
